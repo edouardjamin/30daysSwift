7e0837a8d4b77fb6753b4a60a60a0e42be96e020
edouardjamin <edouard.jamin@gmail.com>
2016-03-03 22:39:18 +0100

    Playback video now loops!

--- Day 10 - SpotifyVideoBackground/Day 10 - SpotifyVideoBackground/ViewController.swift
+++ Day 10 - SpotifyVideoBackground/Day 10 - SpotifyVideoBackground/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController {
 	@IBOutlet weak var loginButton: UIButton!
 	@IBOutlet weak var signupButton: UIButton!
 	
+	// Prototype
+	var player = AVPlayer()
+	var counter :Int = 0
+	var clipDuration :CMTimeValue = 0
+	var clipBeginsAt :CMTime = CMTime()
+	
 	// Video setting
 	let videoURL :NSURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("moments", ofType: "mp4")!)
 	
@@ -30,13 +36,16 @@ class ViewController: UIViewController {
 		
 		let f = CGRectMake(0, 0, self.mainView.frame.size.width, self.mainView.frame.size.height)
 		let playItem = AVPlayerItem(URL: videoURL)
-		let player = AVPlayer(playerItem: playItem)
+		player = AVPlayer(playerItem: playItem)
 		let playerLayer = AVPlayerLayer(player: player)
 		
 		self.videoView.frame = f
 		playerLayer.frame = f
 		videoView.layer.addSublayer(playerLayer)
 		player.play()
+		
+		// NSTimer for looping
+		_ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("getCurrentTime"), userInfo: nil, repeats: true)
 		
 		
 		// Do any additional setup after loading the view, typically from a nib.
@@ -50,6 +59,31 @@ class ViewController: UIViewController {
 	// Status bar
 	override func preferredStatusBarStyle() -> UIStatusBarStyle {
 		return UIStatusBarStyle.LightContent
+	}
+	
+	func getCurrentTime() {
+		// Get video infos
+		let currentItem :AVPlayerItem = player.currentItem!
+		let currentTime :CMTime = currentItem.currentTime()
+		
+		// Get CMTime of the begging of the video
+		if counter == 0 {
+			clipBeginsAt = currentTime
+		}
+		
+		// Get clip duration
+		if counter == 1 {
+			clipDuration = currentItem.duration.value
+		}
+		
+		// Loop video when it ends
+		if (currentTime.value == clipDuration) {
+			player.seekToTime(clipBeginsAt)
+			player.play()
+		}
+		
+		// Taht's all folks!
+		counter++
 	}
 
 
